586682a9ce8adeb6e815fc991a7d1f75b1566d77
Manoj M <73076286+MANOJ-M-01@users.noreply.github.com>
2021-02-05 15:10:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const Joi = require('@hapi/joi');
 const movies = require('./movies');
-
+const fs = require('fs');
 app.use(express.json());
 
 app.use('/abc', movies);
@@ -13,6 +13,10 @@ app.get('/', (req,res) => {
 })
 
 
+const __dirname=fs.realpathSync('.');
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname + "/404.html")
+  });
 
 
 
